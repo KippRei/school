d472d0525d7704eef8c807e7118d84864d987294
kipp <kippreitzel@gmail.com>
2023-09-18 14:28:41 -0700

    first commit

---
+++ quadeq.py
@@ -0,0 +1,18 @@
+#Quadratic Formula
+import math
+
+def qe(a,b,c):
+    x1 = -b + math.sqrt((b**2 - 4*(a*c)))
+    x1 = x1 / (2*a)
+    x2 = -b - math.sqrt((b**2 - 4*(a*c)))
+    x2 = x2 / (2*a)
+    print(x1, " and ", x2)
+
+val = input("Enter values: ")
+val = val.split(",")
+        
+a = float(val[0])
+b = float(val[1])
+c = float(val[2])
+
+qe(a,b,c)
