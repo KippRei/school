642986c35d67d6a14080e51b9338d4f81030dd7b
kipp <kippreitzel@gmail.com>
2023-09-25 12:29:21 -0700

    quadeq.py: made output slightly easier to read

--- quadeq.py
+++ quadeq.py
@@ -6,7 +6,7 @@ def qe(a,b,c):
     x1 = x1 / (2*a)
     x2 = -b - math.sqrt((b**2 - 4*(a*c)))
     x2 = x2 / (2*a)
-    print(x1, " and ", x2)
+    print("= ", x1, " and ", x2)
 
 val = input("Enter values: ")
 val = val.split(",")
