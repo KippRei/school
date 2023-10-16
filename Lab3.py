6cbae2018b070103c8a1449bbef13b0024e8fa53
kipp <kippreitzel@gmail.com>
2023-10-16 11:52:18 -0700

    lab3 tweaks

--- Lab3.py
+++ Lab3.py
@@ -1,26 +1,28 @@
-#TODO: make switch function
-
-#TODO: for testing
-calcType = "add"
-
 def Get2Numbers():
     numbers = input("Please enter two numbers: ")
     firstNum, secondNum = [float(x) for x in numbers.split()]
-    calcType = input("Calc Type?: ").upper()
     return firstNum, secondNum
 
-def DoCalc(firstNum, secondNum, calculation):
-    match calculation:
-        case "ADD":
-            print(firstNum + secondNum)
-        case "SUBTRACT":
-            print(firstNum - secondNum)
-        case _:
-            print("Could not calculate")
+def GetCalcType():
+    calcType = input("Calc Type?: ").upper()
+    return calcType
 
-try:
-    firstNum, secondNum = Get2Numbers()
-    DoCalc(firstNum, secondNum, calcType)
-except:
-    print("I'm sorry, that's an invalid input")
-    Get2Numbers()
+def DoCalc(firstNum, secondNum, calculation):
+    if calculation == "ADD":
+        print(firstNum + secondNum)
+    elif calculation == "SUBTRACT":
+        print(firstNum - secondNum)
+    elif calculation == "MULTIPLY":
+        print(firstNum * secondNum)
+    elif calculation == "DIVIDE":
+        print(firstNum / secondNum)
+    else:
+        print("Cannot calculate")
+while True:
+    try:
+        firstNum, secondNum = Get2Numbers()
+        calcType = GetCalcType()
+        DoCalc(firstNum, secondNum, calcType)
+        break
+    except:
+        print("I'm sorry, that's an invalid input")
