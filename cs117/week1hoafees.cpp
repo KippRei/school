8d2bf3cbb794cdec6e226aacc2633446482e0863
kipp <kippreitzel@gmail.com>
2023-10-29 02:31:31 -0700

    added timer to program

--- cs117/week1hoafees.cpp
+++ cs117/week1hoafees.cpp
@@ -10,40 +10,52 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <chrono>
 
 int main() {
-  // Variables and consts
-  const int yearsProjected = 10;
-  const float feePercentIncrease = .01;
-  const float origMemberFee = 275.00;
-  float memberFeeIncrease = 1.01;
-  float currMemberFee = 275.00;
+    // Variables and consts
+    int yearsProjected = 10;
+    std::string years = "10";
+    const float feePercentIncrease = .01;
+    const float origMemberFee = 275.00;
+    float memberFeeIncrease = 1.01;
+    float currMemberFee = 275.00;
 
-  // Chart heading
-  std::cout << "Year    Increase    Amount\n"
-            << "===========================\n";
+    // Ask for years to calculate
+    std::cout << "Enter years to calculate: ";
+    std::cin >> yearsProjected;
 
-  // Loops {yearsProjected} # of times and displays
-  for (int i = 0; i < yearsProjected; i++) {
-    // Calculate the increased member fee
-    currMemberFee = origMemberFee * memberFeeIncrease;
+    auto start = std::chrono::high_resolution_clock::now();
+    // Chart heading
+    std::cout << "Year    Increase    Amount\n"
+                << "===========================\n";
 
-    // inc is used for year and percent increase
-    int inc = i + 1;
+    // Loops {yearsProjected} # of times and displays
+    for (int i = 0; i < yearsProjected; i++) {
+        // Calculate the increased member fee
+        currMemberFee = origMemberFee * memberFeeIncrease;
 
-    // used to stringstream to concat number with percent sign for proper
-    // display
-    std::stringstream percentInc;
-    percentInc << i + 1 << "%";
+        // inc is used for year and percent increase
+        int inc = i + 1;
 
-    // Display the year, percent increase, and amount
-    std::cout << std::endl;
-    std::cout << std::setw(8) << std::left << inc;
-    std::cout << std::setw(12) << std::left << percentInc.str();
-    std::cout << std::fixed << std::setprecision(2) << "$" << currMemberFee
-              << std::endl;
+        // used to stringstream to concat number with percent sign for proper
+        // display
+        std::stringstream percentInc;
+        percentInc << i + 1 << "%";
 
-    // Increase the percentage used to calculate member fee for next year
-    memberFeeIncrease += feePercentIncrease;
-  }
+        // Display the year, percent increase, and amount
+        std::cout << std::endl;
+        std::cout << std::setw(8) << std::left << inc;
+        std::cout << std::setw(12) << std::left << percentInc.str();
+        std::cout << std::fixed << std::setprecision(2) << "$" << currMemberFee
+                << std::endl;
+
+        // Increase the percentage used to calculate member fee for next year
+        memberFeeIncrease += feePercentIncrease;
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+ 
+    std::cout << "Time taken by function: "
+         << duration.count() << " microseconds" << std::endl;
 }
