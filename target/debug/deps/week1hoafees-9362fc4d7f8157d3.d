23849d344d38aac9340cbd9125a8ece32638c9b1
kipp <kippreitzel@gmail.com>
2023-10-29 02:01:24 -0700

    cs117 stuff

---
+++ target/debug/deps/week1hoafees-9362fc4d7f8157d3.d
@@ -0,0 +1,5 @@
+/home/kipp/repo/target/debug/deps/week1hoafees-9362fc4d7f8157d3.rmeta: cs117/rust117/week1hoafees/src/main.rs
+
+/home/kipp/repo/target/debug/deps/week1hoafees-9362fc4d7f8157d3.d: cs117/rust117/week1hoafees/src/main.rs
+
+cs117/rust117/week1hoafees/src/main.rs:
