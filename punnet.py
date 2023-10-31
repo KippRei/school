7304cfd3f722c7576a57dc0649ff568789ac68f9
kipp <kippreitzel@gmail.com>
2023-10-31 11:27:10 -0700

    genotype and phenotype results display tweaks

--- punnet.py
+++ punnet.py
@@ -1,92 +1,130 @@
-p1 = input("Input P1 Genotype: ")
-p2 = input("Input P2 Genotype: ")
-comboArr = []
+def main():
+    p1 = input("Input P1 Genotype: ")
+    p2 = input("Input P2 Genotype: ")
+    comboArr = []
 
-if len(p1) != len(p2):
-    print("Allele numbers must match")
-    exit()
+    if len(p1) != len(p2):
+        print("Allele numbers must match")
+        exit()
 
-geneLength = len(p1) / 2
-print(geneLength)
-if geneLength % 1 != 0:
-    print("Improper allele length")
-    exit()
+    geneLength = len(p1) / 2
+    DebugPrint(geneLength)
+    if geneLength % 1 != 0:
+        print("Improper allele length")
+        exit()
 
-#combine sections of alleles (2 at a time) and combine into arrays
-i = 0
-while i < geneLength * 2:
-    temp = []
-    for j in range(i, i+2):
-        for k in range(i, i+2):
-             temp.append(p1[j] + p2[k])
-    comboArr.append(temp) 
-    i += 2
-
-i = 0
-f1Arr = []
-iterArr = comboArr
-numInComboArr = len(comboArr)
-numOfItemsInF1Arr = 2
-
-# loop: multiplies genes two at a time (creating arrays) until there is one array remaining
-# i.e. AABBCCDD + aabbccdd => first loop [['AABB', 'AABb', 'AAbB', 'AAbb', 'AaBB', 'AaBb', 'AabB', 'Aabb', 'aABB', 'aABb', 'aAbB', 'aAbb', 'aaBB', 'aaBb', 'aabB', 'aabb']] \
-# second (final) loop [['AABB', 'AABb', 'AAbB', 'AAbb', 'AaBB', 'AaBb', 'AabB', 'Aabb', 'aABB', 'aABb', 'aAbB', 'aAbb', 'aaBB', 'aaBb', 'aabB', 'aabb']]
-
-while numOfItemsInF1Arr > 1:
-    f1Arr = []
-    numInComboArr = len(iterArr)
+    #combine sections of alleles (2 at a time) and combine into arrays
     i = 0
-
-    while i < len(iterArr) - 1:
-        f1TempArr = []
-        tempArr = iterArr[i]
-        for a in range(0,len(iterArr[i])):
-            aVal = tempArr[a]
-            for b in range(0,len(iterArr[i+1])):
-                f1TempArr.append(aVal + iterArr[i+1][b])
-        f1Arr.append(f1TempArr)
+    while i < geneLength * 2:
+        temp = []
+        for j in range(i, i+2):
+            for k in range(i, i+2):
+                temp.append(p1[j] + p2[k])
+        comboArr.append(temp) 
         i += 2
 
-    if i == numInComboArr - 1:
-        f1Arr.append(iterArr[i])
-    
-    iterArr = f1Arr
-    print(iterArr)
-    numOfItemsInF1Arr = len(iterArr)
-    print(numOfItemsInF1Arr)
+    i = 0
+    f1Arr = []
+    iterArr = comboArr
+    numInComboArr = len(comboArr)
+    numOfItemsInF1Arr = 2
 
-print("final", f1Arr)
-print(len(f1Arr[0]))
+    # loop: multiplies genes two at a time (creating arrays) until there is one array remaining
+    # i.e. AABBCCDD + aabbccdd => first loop [['AABB', 'AABb', 'AAbB', 'AAbb', 'AaBB', 'AaBb', 'AabB', 'Aabb', 'aABB', 'aABb', 'aAbB', 'aAbb', 'aaBB', 'aaBb', 'aabB', 'aabb']] \
+    # second (final) loop [['AABB', 'AABb', 'AAbB', 'AAbb', 'AaBB', 'AaBb', 'AabB', 'Aabb', 'aABB', 'aABb', 'aAbB', 'aAbb', 'aaBB', 'aaBb', 'aabB', 'aabb']]
 
-# convert final array into list for easier use
-tempFinalList = []
-for item in f1Arr[0]:
-    tempFinalList.append(item)
+    while numOfItemsInF1Arr > 1:
+        f1Arr = []
+        numInComboArr = len(iterArr)
+        i = 0
+
+        while i < len(iterArr) - 1:
+            f1TempArr = []
+            tempArr = iterArr[i]
+            for a in range(0,len(iterArr[i])):
+                aVal = tempArr[a]
+                for b in range(0,len(iterArr[i+1])):
+                    f1TempArr.append(aVal + iterArr[i+1][b])
+            f1Arr.append(f1TempArr)
+            i += 2
+
+        if i == numInComboArr - 1:
+            f1Arr.append(iterArr[i])
+        
+        iterArr = f1Arr
+        DebugPrint(iterArr)
+        numOfItemsInF1Arr = len(iterArr)
+        DebugPrint(numOfItemsInF1Arr)
+
+    DebugPrint(["final", f1Arr])
+    DebugPrint(len(f1Arr[0]))
+
+    # convert final array into list for easier use
+    tempFinalList = []
+    for item in f1Arr[0]:
+        tempFinalList.append(item)
 
 
-finalList = []
-# TODO: ensure casing is adjusted for comparison (i.e. aA -> Aa), not working correctly
-for it in tempFinalList:
-    g = 0
-    while g < len(it) - 1:
+    finalList = []
+    # TODO: ensure casing is adjusted for comparison (i.e. aA -> Aa), not working correctly
+    for it in tempFinalList:
+        g = 0
         itList = list(it)
-        if itList[g].islower() and itList[g+1].isupper():
-            itList[g].upper()
-            itList[g+1].lower()
-        g += 2
-    newIt = ''.join(itList)
-    finalList.append(newIt)
-    
+        while g < len(it) - 1:
+            if itList[g].islower() and itList[g+1].isupper():
+                itList[g] = itList[g].upper()
+                itList[g+1] = itList[g+1].lower()
+            g += 2
+        DebugPrint(itList)
+        newIt = ''.join(itList)
+        finalList.append(newIt)
 
-    print(newIt)
+
+    finalCount = {}
+    while len(finalList) > 0:
+        itemToCheck = finalList.pop(0)
+        if itemToCheck in finalCount:
+            finalCount[itemToCheck] += 1
+        else:
+            finalCount[itemToCheck] = 1
+
+    DataCalc(finalCount)
+
+def DataCalc(data):
+    print("F1 Genotype:")
+    DictPrint(data)
+
+    print("F1 Phenotype: ")
+    alleleLength = len(list(data.keys())[0]) #get length of allele
+    phenoDict = {}
+    for key, val in data.items():
+        i = 0
+        phenoKey = ""
+        while i < alleleLength:
+            phenoKey += key[i]
+            i += 2
+            
+        if phenoKey in phenoDict:
+            phenoDict[phenoKey] += val
+        else:
+            phenoDict[phenoKey] = val
+
+    DictPrint(phenoDict)
 
 
-finalCount = {}
-while len(finalList) > 0:
-    itemToCheck = finalList.pop(0)
-    if itemToCheck in finalCount:
-        finalCount[itemToCheck] += 1
-    else:
-        finalCount[itemToCheck] = 1
+def DictPrint(dict):
+    strToPrint = ""
+    for key, val in dict.items():
+        strToPrint += str(val) + " " + str(key) + " : \n"
+    tempStrList = list(strToPrint)
+    tempStrList.pop(-3)
+    strToPrint = "".join(tempStrList)
+    #strToPrint = strToPrint.rstrip(strToPrint[-3])
+    print(strToPrint)
 
-print(finalCount)
+def DebugPrint(toPrint):
+    #print(toPrint)
+    return
+
+
+main()
