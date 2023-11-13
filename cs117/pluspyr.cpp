a066c6b0509535310c2b41daecda7b6e32fa20ac
kipp <kippreitzel@gmail.com>
2023-11-12 23:47:30 -0800

    no major changes

--- cs117/pluspyr.cpp
+++ cs117/pluspyr.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    int i = 1;
-    for ( ; i <= 10; i++) {
-        int j = 0;
-        while (j < i) {
-            std::cout<<"+";
-            j++;
-        }
-        std::cout<<std::endl;
-    }
-
-    for (i = 10 ; i >=1 ; i--) {
-        int j = 0;
-        while (j < i) {
-            std::cout<<"+";
-            j++;
-        }
-        std::cout<<std::endl;
-    }
-    return 0; 
+    char greeting[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    std::cout << greeting;
 }
