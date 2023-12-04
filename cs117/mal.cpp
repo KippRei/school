049cb4e0948c4246d523a33e41184557241b47b2
kipp <kippreitzel@gmail.com>
2023-12-03 21:08:34 -0800

    cs117 week 6 stuff

--- cs117/mal.cpp
+++ cs117/mal.cpp
@@ -4,11 +4,26 @@
 #include <string.h>
 
 using namespace std;
+struct Names
 
-int main() {
-char* x = "a", *y = "a";
+{
 
-  if (strcmp(x, y) == 0)
-    cout<<"abdadfs";
-    exit (0);
+   string first;
+
+   string last;
+
+};
+
+int main ()
+
+{
+
+  Names customer = {"asdf", "aa"};
+
+  cout << customer.first << endl;
+
+  cout << customer.last << endl;
+
+return 0;
+
 }
