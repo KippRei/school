95e0e8438dee385bca2cb49e370a5cd1ab0275ed
kipp <kippreitzel@gmail.com>
2023-12-13 18:34:16 -0800

    added file creation/reading to final.cpp

--- cs117/final.cpp
+++ cs117/final.cpp
@@ -6,6 +6,8 @@
 #include <map>
 
 using std::ifstream;
+using std::ofstream;
+using std::fstream;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -24,40 +26,111 @@ struct OperatingCosts {
     float misc;
 };
 
+// Arrays hold keywords for budget file creation/parsing
+const vector<string> fileBudget = {"b_rent", "b_utilities", "b_supplies", "b_tax", "b_insurance", "b_miscellaneous"};
+const vector<string> fileExpense = {"e_rent", "e_utilities", "e_supplies", "e_tax", "e_insurance", "e_miscellaneous"};
+
 // Prototypes
+string start();
 void defaultBudget(OperatingCosts*);
 void budgetFromFile(OperatingCosts*, OperatingCosts*, ifstream*);
-void initializeB_EMap(map<string, float>*, const vector<string>*);
-float convertToFloat(string);
 void getExpensesFromUser(OperatingCosts*);
 void displayResults(OperatingCosts*, OperatingCosts*);
+float getValidNum();
+void createDefaultBudgetFile(string);
+void initializeB_EMap(map<string, float>*, const vector<string>*);
+float convertToFloat(string);
 
 
 // Main
-int main(int argc, char* argv[]) {
+int main() {
     OperatingCosts storeBudget; // Will hold budget for monthly store expenditures
     OperatingCosts storeExpenses; // Will hold actual monthly store expenditures
 
-    if (argc > 1) {
-        ifstream file(argv[1]);
+    bool validFile = false; // Flag for whether or not budget/expense file could be opened (if provided)
+
+    do {
+        string filePath = start();
+
+        if (filePath.size() > 0) {
+            ifstream file(filePath);
+            if (file) {
+                cout << "Reading data from file..." << endl;
+                budgetFromFile(&storeBudget, &storeExpenses, &file);
+                file.close();
+                validFile = true;
+            }
+            else {
+                cout << "Could not read file provided, please try again.\n" << endl;
+            }
+        }
+        else {
+            defaultBudget(&storeBudget);
+            getExpensesFromUser(&storeExpenses);
+            validFile = true;
+        }
+    } while (!validFile);
+
+    displayResults(&storeBudget, &storeExpenses);
+
+    return 0;
+}
+
+
+
+// Displays welcome message with program information and returns filepath if provided (returns empty string otherwise)
+string start() {
+    cout << "Welcome To Your Monthly Expense Calculator" << endl;
+    cout << "Here you can set your budget then enter your expenses to determine if you're over or under budget for the month" << endl;
+    cout << "If you'd like to use the default budget and enter your expenses manually, just press enter.\n";
+    cout << "If you'd like to upload your budget and/or expenses from a file, enter the filepath.\n";
+    cout << "You can also type dfp to use the default filepath (./budget.txt).\n";
+    cout << "(If default filepath does not exist a file will be created and you will be asked to input data).\n";
+    cout << "Input: ";
+    string input = "";
+    getline(cin, input);
+    cout << endl;
+
+    string checkDefault;
+    for (char c: input) checkDefault += toupper(c);
+
+    string filePath = "";
+    if (checkDefault == "DFP") {
+        filePath = "./budget.txt";
+        fstream file(filePath);
         if (file) {
-            budgetFromFile(&storeBudget, &storeExpenses, &file);
             file.close();
         }
         else {
-            cout << "Could not read file provided, using default budget." << endl;
-            defaultBudget(&storeBudget);
-            getExpensesFromUser(&storeExpenses);
-            displayResults(&storeBudget, &storeExpenses);
+            createDefaultBudgetFile(filePath);
         }
     }
-    else {
-        defaultBudget(&storeBudget);
-        getExpensesFromUser(&storeExpenses);
-        displayResults(&storeBudget, &storeExpenses);
+    else if (input.size() > 0) filePath = input;
+
+    return filePath;
+}
+
+
+// Creates a default budget.txt file
+void createDefaultBudgetFile(string filePath) {
+    ofstream file;
+    file.open(filePath);
+    file << "(Budget)";
+    for (string val: fileBudget) {
+        file << "\n" << val << "=";
+        val = val.substr(val.find('_') + 1);
+        cout << "Please manually enter the budget for " << val << ": $";
+        file << getValidNum();
     }
 
-    return 0;
+    file << "\n\n(Expenditures)";
+    for (string val: fileExpense) {
+        file << "\n" << val << "=";
+        val = val.substr(val.find('_') + 1);
+        cout << "Please manually enter the amount spent on " << val << ": $";
+        file << getValidNum();
+    }
+    file.close();
 }
 
 
@@ -76,10 +149,6 @@ void defaultBudget(OperatingCosts* storeBudget) {
 // Reads from file and inserts value if specified in file
 // See budget.txt for format of budget file
 void budgetFromFile(OperatingCosts* storeBudget, OperatingCosts* storeExpenses, ifstream* file) {
-    // Arrays hold keywords to check for in file
-    const vector<string> fileBudget = {"b_rent", "b_utilities", "b_supplies", "b_tax", "b_insurance", "b_miscellaneous"};
-    const vector<string> fileExpense = {"e_rent", "e_utilities", "e_supplies", "e_tax", "e_insurance", "e_miscellaneous"};
-
     map<string, float> budgetMap;
     initializeB_EMap(&budgetMap, &fileBudget);
     map<string, float> expenseMap;
@@ -96,6 +165,7 @@ void budgetFromFile(OperatingCosts* storeBudget, OperatingCosts* storeExpenses, 
                 }
             }
         }
+
         for (map<string, float>::iterator iter = expenseMap.begin(); iter != expenseMap.end(); iter++) {
             if (iter->first.compare(name) == 0) {
                 float convertedNum = convertToFloat(line.substr(line.find('=') + 1));
@@ -103,6 +173,32 @@ void budgetFromFile(OperatingCosts* storeBudget, OperatingCosts* storeExpenses, 
                     iter->second = convertedNum;
                 }
             }
+        }
+    }
+
+    for (map<string, float>::iterator iter = budgetMap.begin(); iter != budgetMap.end(); iter++) {
+        if (iter->second < 0) {
+            float num = -1;
+            while (num < 0) {
+                string item = iter->first;
+                item = item.substr(item.find('_') + 1);
+                cout << "Please manually enter the budget for " << item << ": $";
+                num = getValidNum();
+            }
+            iter->second = num;
+        }
+    }
+    
+    for (map<string, float>::iterator iter = expenseMap.begin(); iter != expenseMap.end(); iter++) {
+        if (iter->second < 0) {
+            float num = -1;
+            while (num < 0) {
+                string item = iter->first;
+                item = item.substr(item.find('_') + 1);
+                cout << "Please manually enter the amount spent on " << item << ": $";
+                num = getValidNum();
+            }
+            iter->second = num;
         }
     }
     
@@ -126,23 +222,17 @@ void budgetFromFile(OperatingCosts* storeBudget, OperatingCosts* storeExpenses, 
 void getExpensesFromUser(OperatingCosts* storeExpenses) {
     string val;
     cout << "Enter the amount spent for rent: $";
-    getline(cin, val);
-    storeExpenses->rent = convertToFloat(val);
+    storeExpenses->rent = getValidNum();
     cout << "Enter the amount spent for utilities: $";
-    getline(cin, val);
-    storeExpenses->utilities = convertToFloat(val);
+    storeExpenses->utilities = getValidNum();
     cout << "Enter the amount spent for supplies: $";
-    getline(cin, val);
-    storeExpenses->supplies = convertToFloat(val);
+    storeExpenses->supplies = getValidNum();
     cout << "Enter the amount spent for tax: $";
-    getline(cin, val);
-    storeExpenses->tax = convertToFloat(val);
+    storeExpenses->tax = getValidNum();
     cout << "Enter the amount spent for insurance: $";
-    getline(cin, val);
-    storeExpenses->insurance = convertToFloat(val);
+    storeExpenses->insurance = getValidNum();
     cout << "Enter the amount spent for miscellaneous: $";
-    getline(cin, val);
-    storeExpenses->misc = convertToFloat(val);
+    storeExpenses->misc = getValidNum();
 }
 
 
@@ -150,6 +240,7 @@ void getExpensesFromUser(OperatingCosts* storeExpenses) {
 void displayResults(OperatingCosts* storeBudget, OperatingCosts* storeExpenses) {
     float totalOverUnder = 0;
 
+    cout << endl;
     cout << std::setw(20) << std::left << "Type of Expense";
     cout << std::setw(13) << std::left << "Budgeted"; 
     cout << std::setw(10) << std::left << "Spent";
@@ -177,13 +268,58 @@ void displayResults(OperatingCosts* storeBudget, OperatingCosts* storeExpenses) 
     overUnder = storeBudget->supplies - storeExpenses->supplies;
     totalOverUnder += overUnder;
     cout << std::setw(15) << std::right << overUnder << endl;
+
+    cout << std::setw(17) << std::left << "Tax";
+    cout << std::setw(10) << std::right << storeBudget->tax; 
+    cout << std::setw(12) << std::right << storeExpenses->tax;
+    overUnder = storeBudget->tax - storeExpenses->tax;
+    totalOverUnder += overUnder;
+    cout << std::setw(15) << std::right << overUnder << endl;
+
+    cout << std::setw(17) << std::left << "Insurance";
+    cout << std::setw(10) << std::right << storeBudget->insurance; 
+    cout << std::setw(12) << std::right << storeExpenses->insurance;
+    overUnder = storeBudget->insurance - storeExpenses->insurance;
+    totalOverUnder += overUnder;
+    cout << std::setw(15) << std::right << overUnder << endl;
+
+    cout << std::setw(17) << std::left << "Miscellaneous";
+    cout << std::setw(10) << std::right << storeBudget->misc; 
+    cout << std::setw(12) << std::right << storeExpenses->misc;
+    overUnder = storeBudget->misc - storeExpenses->misc;
+    totalOverUnder += overUnder;
+    cout << std::setw(15) << std::right << overUnder << endl;
+
+    if (totalOverUnder > 0) {
+        cout << "For the month you are under budget by $" << totalOverUnder << endl;
+    }
+    else if (totalOverUnder < 0) {
+        cout << "For the month you are over budget by -$" << -totalOverUnder << endl;
+    }
+    else {
+        cout << "For the month you are right on budget." << endl;
+    }
+}
+
+
+// Gets valid number from user
+float getValidNum() {
+    string val;
+    float num = -1;
+    while (num < 0) {
+        getline(cin, val);
+        num = convertToFloat(val);
+        if (num < 0) cout << "Please enter a number value 0 or greater: $";
+    }
+
+    return num;
 }
 
 // Takes a map<string, float> and a pointer to a vector that holds map keys
 // Initializes a budget or expense map with keys from vector and default value 0
 void initializeB_EMap(map<string, float>* beMap, const vector<string>* values) {
     for (string name: *values) {
-        beMap->insert({name, 0});
+        beMap->insert({name, -1});
     }
 }
 
@@ -191,8 +327,12 @@ void initializeB_EMap(map<string, float>* beMap, const vector<string>* values) {
 // Attempts to convert string to float, returns -1 if fail
 float convertToFloat(string num) {
     float convertedNum = 0;
+    size_t index;
     try {
-        convertedNum = stof(num);
+        convertedNum = stof(num, &index);
+        if (index != num.size()) {
+            throw(index);
+        }
     }
     catch(...) {
         convertedNum = -1;
