9b2ddc31f9d11574ebd12114c66edca4aa6d869e
kipp <kippreitzel@gmail.com>
2024-02-11 15:21:56 -0800

    added ch13.2 to cs216

--- cs117/mal.cpp
+++ cs117/mal.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
 
 int main() {
-  float finalScr = 0;
-
-  for (float i = 70; i < 90; i += 0.5) {
-    float exam1 = 97;
-    float exam2 = 95;
-    float exam3 = 78.5;
-    finalScr = i;
-    float avg = (exam1 + exam2 + exam3) / 3;
-    if (finalScr > exam3) {
-      exam3 = (exam1 + exam2 + finalScr)/3;
-      avg = (exam1 + exam2 + exam3) / 3;
-    }
-    else {
-      finalScr = avg;
-    }
-    std::cout << "Final Score: " << finalScr << "\tTest Average: " << avg << "\t3rd Exam: " << exam3 << std::endl;
-  }
-
+  int x = 1;
+  int y = 1;
+  std::cout << x+y << std::endl;
   return 0;
 }
