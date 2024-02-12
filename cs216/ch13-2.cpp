dad7d3f597234e6a48fdf4c1c51fb0ef5dede848
kipp <kippreitzel@gmail.com>
2024-02-11 16:04:59 -0800

    working on sort and search algos

--- cs216/ch13-2.cpp
+++ cs216/ch13-2.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+
+const int MAX_USER_INPUT = 100;
+
 //define Contact structure here
+struct Contact {
+    char name[MAX_USER_INPUT];
+    char phone_number[MAX_USER_INPUT];
+};
+
 //declare your sort() and binSearch() functions
+void sort(Contact* contacts, int contactSize) {
+
+}
+
+void binSearch()
+
 int main() {
-   //your program goes here.
-   return 0;
+    //your program goes here.
+    int numOfContacts;
+    cout << "Enter number of people: ";
+    cin >> numOfContacts;
+    Contact* contacts = new Contact[numOfContacts];
+    
+    for (int i = 0; i < numOfContacts; i++) {
+        cout << "Enter contact name: ";
+        cin.ignore();
+        cin.getline(contacts[i].name, MAX_USER_INPUT);
+
+        cout << "Enter contact number: ";
+        cin.ignore();
+        cin.getline(contacts[i].phone_number, MAX_USER_INPUT);
+    }
+
+    sort(contacts, numOfContacts);
+
+    cout << "You entered " << contacts[0].name;
+
+    delete(contacts);
+    return 0;
 }
