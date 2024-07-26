4fd053d6b1c38169977d2563bbfc1c1d4127451d
kipp <kippreitzel@gmail.com>
2024-07-26 02:39:00 -0700

    update repo

---
+++ rustprac/fstring/target/debug/deps/fstring-bf451bea9a8e1e11.d
@@ -0,0 +1,5 @@
+/home/kipp/repo/rustprac/fstring/target/debug/deps/fstring-bf451bea9a8e1e11: src/main.rs
+
+/home/kipp/repo/rustprac/fstring/target/debug/deps/fstring-bf451bea9a8e1e11.d: src/main.rs
+
+src/main.rs:
---
+++ rustprac/practice/target/debug/practice.d
@@ -0,0 +1,1 @@
+/home/kipp/repo/school/rustprac/practice/target/debug/practice: /home/kipp/repo/school/rustprac/practice/src/main.rs /home/kipp/repo/school/rustprac/practice/src/notmain.rs
